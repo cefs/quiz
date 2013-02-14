62250ddf2e96df8baf220a2ea46e687974679e86
CARLOS SILVA <cefs1984@gmail.com>
2013-02-14 01:59:26 -0200

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quiz::Application.config.secret_token = 'fb24bfb90840dd0123fcaa246cc90b8225e58b6416e1d630573070d6707d5d7072a2fa3bdce8192708793508a98feb7e20a935a1b240c1101bf31c1a27bb639d'
