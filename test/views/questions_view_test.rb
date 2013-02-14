eb5508603003e30e919851b18652e74b95002600
CARLOS SILVA <cefs1984@gmail.com>
2013-02-14 03:02:11 -0200

    Tratamento da view questions/index quando há quesstões cadastradas

--- test/views/questions_view_test.rb
+++ test/views/questions_view_test.rb
@@ -20,8 +20,30 @@ describe "Questions View Test" do
 		end
 
 		describe "with saved questions" do
-			it "omits empty message"
-			it "shows table"
+
+			before do
+				@questions = (1..5).each.collect {|i|
+					question = Question.new title: "Title #{i}",
+						content: "Mussum Ipsum #{i}", difficulty: i
+
+					#simulando uma questão salva no banco de dados, logo, precisa ter um id
+					question.id = i
+					question
+				}
+				render file: "questions/index"
+			end
+
+			it "omits empty message" do
+				assert_select "p.info", count: 0,
+					text: "Nenhuma questao cadastrada"
+			end
+
+			it "shows table" do
+				assert_select "table" do
+					assert_select "thead tr", 1
+					assert_select "tbody tr", @questions.count
+				end
+			end
 		end
 	end
 end
